ccd5b97c71841b0307c21f0936a06ec509791850
Vikash Sharma <expertvikash@gmail.com>
2024-10-13 21:10:38 +0530

    jwt done

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,8 @@
 import { Response, Request } from "express";
 import { prismaClient } from "..";
 import { hashSync, compareSync } from "bcrypt";
-
+import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets";
 
 export const signup = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
@@ -23,22 +24,30 @@ export const signup = async (req: Request, res: Response) => {
   });
 };
 
-
-
 export const login = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-  
-    let user = await prismaClient.user.findFirst({ where: { email } });
-    if (!user) {
-      throw Error("invalid email or password");
-    }
-   
-    const isPasswordValid = compareSync(password, user.password);
-    if (!isPasswordValid) {
-      throw Error("Invalid email or password" );
-    }
+  const { email, password } = req.body;
 
-    
-  
-    res.status(200).json({ message: "User logged in successfully", user });
-  };
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (!user) {
+    throw Error("invalid email or password");
+  }
+
+  const isPasswordValid = compareSync(password, user.password);
+  if (!isPasswordValid) {
+    throw Error("Invalid email or password");
+  }
+
+  const token = jwt.sign(
+    {
+      username: user.email,
+    },
+    JWT_SECRET
+  );
+
+  res.status(200).json({ 
+    message: "User logged in successfully",
+    user:user.name,
+    token:token
+
+ });
+};
